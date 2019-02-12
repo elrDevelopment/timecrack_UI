ef940ec8510d908ef1d863dc6f96c61aaf58e65f
Erick <erick@pop-os.localdomain>
2019-02-11 23:11:37 -0600

    initial detial frame prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from '@sumoworker/home/home.component';
+import { DetailCardComponent } from '@sumoworker/detail-card/detail-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailCardComponent
   ],
   imports: [
     BrowserModule,
