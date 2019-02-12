ffa3e843d5126e74d7bad53eb5f5ef6786a57141
Erick <erick@pop-os.localdomain>
2019-02-12 12:42:25 -0600

    delete detail-card as non descriptive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from '@sumoworker/home/home.component';
 import { DetailCardComponent } from '@sumoworker/detail-card/detail-card.component';
+import { GoalCardComponent } from './components/goal-card/goal-card.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { GoalDetailComponent } from './components/goal-detail/goal-detail.component';
+import { SubTaskDetailComponent } from './components/sub-task-detail/sub-task-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DetailCardComponent
+    DetailCardComponent,
+    GoalCardComponent,
+    TaskCardComponent,
+    TaskDetailComponent,
+    GoalDetailComponent,
+    SubTaskDetailComponent
   ],
   imports: [
     BrowserModule,
